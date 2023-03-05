b6cef2171a552c3e6eb47e36e903012e7f91380f
mtanzella <g1pd78@gmail.com>
2023-03-05 16:21:56 +0300

    rss feed latest posts and most commented posts markdown

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,6 +5,8 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
+from django.db.models import Count
 
 
 class PostListView(ListView):
@@ -23,11 +25,19 @@ def post_detail(request, year, month, day, post):
                              publish__day=day)
     comments = post.comments.filter(active=True)
     form = CommentForm()
+
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                  .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                 .order_by('-same_tags', '-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                   'comments': comments,
-                  'form': form})
+                  'form': form,
+                  'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
@@ -52,10 +62,16 @@ def post_share(request, post_id):
                                                     'sent':sent})
 
        
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
+
     try:
         posts = paginator.page(page_number)
     except PageNotAnInteger:
@@ -65,7 +81,8 @@ def post_list(request):
 
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'posts': posts,
+                   'tag': tag})
 
 
 @require_POST
